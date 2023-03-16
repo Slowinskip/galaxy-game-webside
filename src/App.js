957c8ad6490eca50dac7d4e918d4a5bbbe3b9210
Patryk Słowiński <patryk_slowinski@o2.pl>
2023-03-16 10:35:07 +0100

    Add component About

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Router, Routes } from 'react-router-dom'
 import Navbar from './components/layout/Navbar/Navbar'
+import About from './components/views/About/About'
 import Home from './components/views/Home/Home'
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   )
---
+++ src/components/views/About/About.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import styles from './About.module.scss'
+const About = () => {
+  return <Container className={styles.row}>About</Container>
+}
+
+export default About
